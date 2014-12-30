95c8e7f6caf23e9ef0c5b92d2229437ffd20f947
Javier Lorenzana <skqr.io@gmail.com>
2014-12-30 16:26:23 -0300

    New classes.

---
+++ Root/MapCollection.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @copyright 2014 Integ S.A.
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @author Javier Lorenzana <javier.lorenzana@gointegro.com>
+ */
+
+namespace GoIntegro\Raml\Root;
+
+class MapCollection implements \Countable, \IteratorAggregate
+{
+    /**
+     * @var array
+     */
+    private $maps = [];
+
+    /**
+     * @param array $map
+     * @return self
+     */
+    public function add(Map $map)
+    {
+        $this->maps[] = $map;
+    }
+
+    /**
+     * @param string $name
+     * @return \stdClass|NULL
+     */
+    public function has($name)
+    {
+        foreach ($this->maps as $map) {
+            foreach ($map as $key => $schema) {
+                if ($key === $name) return TRUE;
+            }
+        }
+
+        return FALSE;
+    }
+
+    /**
+     * @param string $name
+     * @return \stdClass|NULL
+     */
+    public function get($name)
+    {
+        foreach ($this->maps as $map) {
+            foreach ($map as $key => $schema) {
+                if ($key === $name) return $schema;
+            }
+        }
+
+        return NULL;
+    }
+
+    /**
+     * @see \Countable::count
+     */
+    public function count()
+    {
+        return count($this->maps);
+    }
+
+    /**
+     * @see \IteratorAggregate::getIterator
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->maps);
+    }
+}
